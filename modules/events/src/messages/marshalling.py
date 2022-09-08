9c6f7a9da55d3a18c55078f3d2db5e12104266b9
Eduardo Neira <edun327@gmail.com>
2022-09-08 15:45:01 -0300

    First modules, still working on marshalling with avro

---
+++ modules/events/src/messages/marshalling.py
@@ -0,0 +1,11 @@
+import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+
+
+def decode_message(data):
+    return
+
+
+def encode_message(encoded_data):
+    return
