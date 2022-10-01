9f18b8cc8da42c12b29edbb4cb43180126dafa40
Eduardo Neira <aneira@fi.uba.ar>
2022-10-01 15:32:59 -0300

    Minimal fuctionality

--- web/public/main.js
+++ web/public/main.js
@@ -1,61 +1,27 @@
 import { SocketClient } from './modules/socket_client.js'
-import { VideoIndex } from './modules/video_index.js'
-import { VideoCanvas } from './modules/video_canvas.js'
+import { setUpDisplay } from './modules/video_display.js'
 import { VideoBuffer } from './modules/video_buffer.js'
 
 
 window.addEventListener('load', () => {
-  const socketClient = new SocketClient()
-  const videoIndex = new VideoIndex()
-  socketClient.onAnalysis(videoIndex.addAnalysis)
+  const socketClient = new SocketClient();
+  const mediaSource = new MediaSource;
 
-  const videoCanvas = new VideoCanvas(videoIndex)
-
-  const mime = 'video/mp4; codecs="avc1.f4001f"'
-  // eslint-disable-next-line no-undef
-  const mediaSource = new MediaSource()
-
-  const video = document.getElementById('auxiliary-video')
-  video.src = URL.createObjectURL(mediaSource)
+  const video = document.getElementById('auxiliary-video');
+  video.src = URL.createObjectURL(mediaSource);
 
   mediaSource.addEventListener('sourceopen', (e) => {
-    const mediaSource = e.target
-    // Set duration for infinite stream
-    mediaSource.duration = Number.POSITIVE_INFINITY
+    setUpDisplay();
 
-    const sourceBuffer = mediaSource.addSourceBuffer(mime)
-    const videoBuffer = new VideoBuffer(sourceBuffer, videoIndex)
+    const videoBuffer = new VideoBuffer(mediaSource);
 
-    let hasSwitchedTabs = false
+    socketClient.onVideoChunk((chunk) => {
+      videoBuffer.append(chunk);
 
-    function resume () {
       if (video.paused) {
-        video.play()
+        video.play();
       }
-    }
-
-    window.onblur = () => {
-      hasSwitchedTabs = true
-    }
-
-    window.onfocus = () => {
-      if (hasSwitchedTabs) {
-        hasSwitchedTabs = false
-        const lastChunk = videoBuffer.getLastChunk()
-        // TODO: Check time difference to force canvas update. Maybe 10 seconds diff?
-        if (lastChunk != null) {
-          video.currentTime = videoBuffer.getTimeAppended() - lastChunk.duration
-          videoCanvas.updateToChunk(lastChunk.id)
-          resume()
-        }
-      }
-    }
-
-    socketClient.onChunk((chunk) => {
-      if (videoBuffer.preBufferChunk(chunk)) {
-        resume()
-      }
-    })
+    });
   })
 })
 
--- web/public/modules/video_buffer.js
+++ web/public/modules/video_buffer.js
@@ -1,79 +1,16 @@
-import { PriorityQueue } from './priority_queue.js'
-
-// Time to pre-buffer
-const PRE_BUFFER_SIZE = 1.0
+const MIME = 'video/mp4; codecs="avc1.f4001f"';
 
 export class VideoBuffer {
-  constructor (sourceBuffer, videoIndex) {
-    this.sourceBuffer = sourceBuffer
-    this.sourceBuffer.mode = 'sequence'
-
-    this.videoIndex = videoIndex
-
-    this.timeAppended = 0
-    this.preBuffering = 0
-    this.lastTimestamp = 0
-    this.lastChunk = null
-
-    this.queue = new PriorityQueue(function (a, b) {
-      return a.timestamp > b.timestamp
-    })
-
-    // Rename this to use inside callbacks
-    const self = this
-
-    this.sourceBuffer.addEventListener('updateend', function (e) {
-      if (!self.needsToPreBuffer()) {
-        self.appendToBuffer()
-      }
-    })
+  constructor (mediaSource) {
+    this.sourceBuffer = mediaSource.addSourceBuffer(MIME);
+    this.sourceBuffer.mode = 'sequence';
 
     this.sourceBuffer.addEventListener('error', function (e) {
-      console.log(e)
-    })
+      console.log(e);
+    });
   }
 
-  needsToPreBuffer () {
-    return this.preBuffering < PRE_BUFFER_SIZE
-  }
-
-  getTimeAppended () {
-    return this.timeAppended
-  }
-
-  getLastChunk () {
-    return this.lastChunk
-  }
-
-  appendToBuffer () {
-    let chunk
-
-    do {
-      chunk = this.queue.pop()
-      this.preBuffering -= chunk.duration / 1000
-      // Remove chunks that have a timestamp before the last appended until queue is emptied
-    } while (chunk.timestamp < this.lastTimestamp && !this.queue.isEmpty())
-
-    if (chunk.timestamp > this.lastTimestamp) {
-      this.lastTimestamp = chunk.timestamp
-      this.timeAppended += chunk.duration / 1000
-      this.lastChunk = chunk
-      this.videoIndex.addVideoChunk(chunk)
-      this.sourceBuffer.appendBuffer(chunk.payload)
-    }
-  }
-
-  preBufferChunk (chunk) {
-    let appended = false
-
-    this.preBuffering += chunk.duration / 1000
-    this.queue.push(chunk)
-
-    if (!this.sourceBuffer.updating && !this.needsToPreBuffer()) {
-      this.appendToBuffer()
-      appended = true
-    }
-
-    return appended
+  append(chunk) {
+    this.sourceBuffer.appendBuffer(chunk.payload);
   }
 }
