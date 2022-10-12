98ab802ce51d258b2985f0841cc8a86fa5c2689b
Gabriel Gayoso <ga-yo-so@hotmail.com>
2022-10-12 02:07:58 -0300

    added trace id to face messages

--- utils/events/src/messages/face_message.py
+++ utils/events/src/messages/face_message.py
@@ -1,17 +1,19 @@
 class FaceMessage:
 
-    def __init__(self, video_chunk_id, offset, face_num, bounding_box):
+    def __init__(self, video_chunk_id, offset, face_num, bounding_box, trace):
         self.video_chunk_id = video_chunk_id
         self.offset = offset
         self.face_num = face_num
         self.bounding_box = bounding_box
+        self.trace = trace
 
     def to_json(self):
         return {
             'video_chunk_id': self.video_chunk_id,
             'offset': self.offset,
             'face_num': self.face_num,
-            'bounding_box': self.bounding_box
+            'bounding_box': self.bounding_box,
+            'trace': self.trace
         }
 
     def __str__(self):
--- web/public/modules/video_buffer.js
+++ web/public/modules/video_buffer.js
@@ -34,6 +34,7 @@ export class VideoBuffer {
         let hold_0_offset = -1;
         let next_offset = -1;
 
+
         // we iterate the sampled frame offsets for this video chunk
         for (let sample_index = 0; sample_index < samples_per_chunk; ++sample_index) {
             // IMPORTANTE: la logica que calcula los sample frame offsets tiene que ser la misma que en python
@@ -43,7 +44,7 @@ export class VideoBuffer {
                 if (hold_0_offset === -1) {
                     // the current frame is earlier than the first sampled frame from this chunk
                     // so we use the last sampled frame from the previous chunk
-                    if (chunk_index - 1 > 0) {
+                    if (chunk_index - 1 >= 0) {
                         chunk_id = this.chunksMetadata[chunk_index - 1].video_chunk_id;
                         hold_0_offset = Math.round(frames_between_samples / 2) + (samples_per_chunk - 1) * frames_between_samples;
                     }
