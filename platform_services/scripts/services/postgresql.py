1f588396edb77e300f1397e2b6e06662b28881c7
Gabriel Gayoso <ga-yo-so@hotmail.com>
2022-10-30 22:46:02 -0300

    separated services host as config parameter

--- platform_services/scripts/services/postgresql.py
+++ platform_services/scripts/services/postgresql.py
@@ -1,15 +1,15 @@
 from utils.orm.src.database import connect
 from utils.orm.src.management import create_tables, drop_tables
 
-from utils.orm.src.models.camera import Camera
+from utils.orm.src.models import Camera, Person
 
-db = connect('argus', 'localhost', 5432, 'argus', 'panoptes')
+#db = connect('argus', 'localhost', 5432, 'argus', 'panoptes')
 
 
 class PostgresqlService:
 
-    def __init__(self):
-        self.db = connect('argus', 'localhost', 5432, 'argus', 'panoptes')
+    def __init__(self, host):
+        self.db = connect('argus', host, 5432, 'argus', 'panoptes')
 
     def setup(self):
         # Create all the tables from the models
@@ -24,15 +24,11 @@ class PostgresqlService:
                latitude=-34.61743,
                longitude=-58.36827).save()
 
-        # Create initial camera data for mock
-        Camera(alias='camera-mock',
-               mac=0,
-               width=640,
-               height=480,
-               framerate=30,
-               latitude=-34.61743,
-               longitude=-58.36827).save()
+        Person.insert(id=0, name='gonza', dni='33333333').execute()
+        Person(name='gabo', dni='11111111').save()
+        Person(name='edu', dni='22222222').save()
+
 
     def clean(self):
         drop_tables(self.db)
-        self.setup()
+        #self.setup()
