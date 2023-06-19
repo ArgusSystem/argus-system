e986dd2760e150e21c0f284f0ae1db1dae050c31
Eduardo Neira <aneira@fi.uba.ar>
2023-06-19 04:02:15 -0300

    Warden executes rules

--- utils/metadata/src/rules/rule.py
+++ utils/metadata/src/rules/rule.py
@@ -9,14 +9,15 @@ WHEN = 'when'
 
 class Rule:
 
-    def __init__(self, _id, configuration):
-        self._id = _id
+    def __init__(self, _id, configuration, last_update):
+        self.id = _id
         self.who_rule = WhoRule(configuration[WHO])
         self.where_rule = WhereRule(configuration[WHERE])
         self.when_rule = WhenRule(configuration[WHEN])
+        self.last_update = last_update
 
-    def match(self, person, camera, timestamp):
-        return self.who_rule.match(Who(person.id, person.role.id)) and \
-            self.where_rule.match(Where(camera.id, camera.area.id, camera.area.area_type.id)) and \
+    def match(self, person, role, camera, area, area_type, timestamp):
+        return self.who_rule.match(Who(person, role)) and \
+            self.where_rule.match(Where(camera, area, area_type)) and \
             self.when_rule.match(timestamp)
 
---
+++ warden/src/rule_manager.py
@@ -0,0 +1,31 @@
+from utils.metadata.src.rules import Rule
+from utils.orm.src.models import Area, Camera, Face, Person, Restriction, VideoChunk
+
+
+def _fetch_rules():
+    return [Rule(restriction.id, restriction.rule, restriction.last_update) for restriction in Restriction.select()]
+
+
+def _get_face(face_id):
+    return Face.select(Face, Person, VideoChunk, Camera, Area) \
+        .join(Person).switch(Face).join(VideoChunk).join(Camera).join(Area) \
+        .where(Face.id == face_id) \
+        .get()
+
+
+class RuleManager:
+
+    def __init__(self):
+        self.rules = _fetch_rules()
+
+    def execute(self, face_id):
+        face = _get_face(face_id)
+
+        return map(lambda rule: rule.id,
+                   filter(lambda rule: rule.match(person=face.person.id,
+                                                  role=face.person.role_id,
+                                                  camera=face.video_chunk.camera.id,
+                                                  area=face.video_chunk.camera.area.id,
+                                                  area_type=face.video_chunk.camera.area.type_id,
+                                                  timestamp=face.timestamp),
+                          self.rules))
