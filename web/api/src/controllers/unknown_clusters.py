0d8f9dc826c459e7ed6bf1789261a4eb1d4e092d
Eduardo Neira <aneira@fi.uba.ar>
2023-07-10 16:23:55 -0300

    Clusters limit

--- web/api/src/controllers/unknown_clusters.py
+++ web/api/src/controllers/unknown_clusters.py
@@ -1,16 +1,23 @@
+from flask import request
 from peewee import fn
 
 from utils.orm.src.models import UnknownFace
 
 
 def _get_unknown_clusters():
+    clusters_count = request.args.get('count')
+    assert clusters_count
+
+    faces_count = fn.COUNT(UnknownFace.face_id)
+
     return [{
         'id': face.cluster_id,
         'faces_count': face.faces_count
     } for face in UnknownFace
-        .select(UnknownFace.cluster_id, fn.COUNT(UnknownFace.face_id).alias('faces_count'))
+        .select(UnknownFace.cluster_id, faces_count.alias('faces_count'))
         .group_by(UnknownFace.cluster_id)
-        .order_by(UnknownFace.cluster_id.desc())]
+        .order_by(faces_count.desc())
+        .limit(clusters_count)]
 
 
 class UnknownClustersController:
--- web/public/modules/api/unknown_clusters.js
+++ web/public/modules/api/unknown_clusters.js
@@ -1,7 +1,7 @@
-import { API_URL } from './url.js'
+import { API_URL, with_params } from './url.js';
 
-export function fetchUnknownClusters() {
-    return fetch(`${API_URL}/unknown_clusters`)
+export function fetchUnknownClusters(count) {
+    return fetch(with_params(`${API_URL}/unknown_clusters`, {count}))
 		.then((response) => response.json())
 		.catch((error) => console.error('Failed to fetch unknown clusters!', error));
 }
