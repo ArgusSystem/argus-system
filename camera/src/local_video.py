49a093021e1b0d84a5f2f5529edd7a69541a8341
Eduardo Neira <aneira@fi.uba.ar>
2023-11-10 21:59:13 -0300

    Minor changes to allow multistream

--- camera/src/local_video.py
+++ camera/src/local_video.py
@@ -1,9 +1,10 @@
 import os
 from tempfile import gettempdir
+from uuid import uuid4
 
 
 def get_filepath(timestamp, encoding):
-    return os.path.join(gettempdir(), f'{timestamp}.{encoding}')
+    return os.path.join(gettempdir(), f'{timestamp}_{uuid4()}.{encoding}')
 
 
 def delete_video(filepath):
