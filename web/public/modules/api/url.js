5d72a5ad7dc26d11da8e0e1fc0ce154b0883865b
Gabriel Gayoso <ga-yo-so@hotmail.com>
2023-11-11 21:56:31 -0300

    changed references to 'localhost' to 'argus'

--- web/public/modules/api/url.js
+++ web/public/modules/api/url.js
@@ -1,6 +1,7 @@
 const {protocol, hostname} = window.location;
 
-export const API_URL = `${protocol}//${hostname}:5000`;
+//export const API_URL = `${protocol}//${hostname}:5000`;
+export const API_URL = `${protocol}//argus:5000`;
 
 export function with_params(url, params) {
     const endpoint = new URL(url);
