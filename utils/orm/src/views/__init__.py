45ac31393843f2ad914bf1824d70faeea29f6fe9
Eduardo Neira <aneira@fi.uba.ar>
2023-11-22 00:47:22 -0300

    Removed unnecesary import

--- utils/orm/src/views/__init__.py
+++ utils/orm/src/views/__init__.py
@@ -1,5 +1,4 @@
 from .view import View
 from .sighting import Sighting
-from .sighting_readable import SightingReadable
 from .unknown_sighting import UnknownSighting
 from .notification_summary import NotificationSummary
