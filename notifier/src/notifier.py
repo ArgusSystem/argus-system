49f61060835e88fddae9600e753328e8711212f4
Gabriel Gayoso <ga-yo-so@hotmail.com>
2023-11-22 00:22:23 -0300

    Notifier no longer keeps notification history

--- notifier/src/notifier.py
+++ notifier/src/notifier.py
@@ -1,23 +1,21 @@
 from utils.metadata.src.repository.sightings import get_sighting_for
-from .notification_history import NotificationHistory
+#from .notification_history import NotificationHistory
 from utils.metadata.src.repository.notifications import create_notification, get_wardens, get_offenders
 from utils.events.src.messages.broken_restriction_message import BrokenRestrictionMessage
 from utils.events.src.messages.marshalling import decode
 from utils.tracing.src.tracer import get_context
 
 
-def _notify(users, history_entry, broken_restriction_id):
+def _notify(users, broken_restriction_id):
     for user_id in users:
-        if not history_entry.has_notified(user_id):
-            create_notification(user_id, broken_restriction_id)
-            history_entry.notify_user(user_id)
+        create_notification(user_id, broken_restriction_id)
 
 
 class Notifier:
 
     def __init__(self, tracer):
         self.tracer = tracer
-        self.notification_history = NotificationHistory()
+        #self.notification_history = NotificationHistory()
 
     def on_broken_rule(self, message):
         broken_restriction_message: BrokenRestrictionMessage = decode(BrokenRestrictionMessage, message)
@@ -29,9 +27,9 @@ class Notifier:
         sighting = get_sighting_for(face_id, restriction_id)
 
         with self.tracer.start_as_current_span('notifier', context=get_context(broken_restriction_message.trace_id)):
-            history_entry = self.notification_history.update(sighting)
+            #history_entry = self.notification_history.update(sighting)
 
             if sighting.offender():
-                _notify(get_offenders(sighting.offender()), history_entry, broken_restriction_id)
+                _notify(get_offenders(sighting.offender()), broken_restriction_id)
 
-            _notify(get_wardens(restriction_id), history_entry, broken_restriction_id)
+            _notify(get_wardens(restriction_id), broken_restriction_id)
