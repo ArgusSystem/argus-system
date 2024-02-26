b6013fceca9f5707cbeb59373998918cc578ecef
Gabriel Gayoso <ga-yo-so@hotmail.com>
2024-02-26 15:57:40 -0300

    added ssl to argus web

--- web/server.js
+++ web/server.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
-const server = require('./app');
+const createServer = require('./app');
 const configuration = require('./app/configuration');
 const logger = require('./app/logger');
 
 const port = configuration['server']['port'];
+const certificatePath = configuration['server']['ssl_cert_path'];
+const privateKeyPath = configuration['server']['ssl_key_path'];
+
+// Create server
+const server = createServer(certificatePath, privateKeyPath);
 
 // Start server
 server.listen(port, function () {
